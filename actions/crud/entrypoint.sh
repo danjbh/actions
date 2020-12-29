e2113f96544a051e4b84b4981e86927e35644f51
Dan Meyers <danjbh@users.noreply.github.com>
2020-12-28 19:48:14 -0800

    Tweaking entrypoint and adding a dummy workflow

--- actions/crud/entrypoint.sh
+++ actions/crud/entrypoint.sh
@@ -7,10 +7,10 @@ echo VERSION: $INPUT_VERSION
 echo WORKFLOWS: $INPUT_WORKFLOWS
 echo CATALOG: $INPUT_CATALOG
 
-# git clone --branch ${VERSION} ${REPO}
-# cd actions # This needs to change... obviously.
+git clone --branch ${VERSION} ${REPO}
+cd actions # This needs to change... obviously.
 
-# for workflow in $(tr , ' ' <<<${WORKFLOWS}); do 
-#   # Install the workflow
-#   cp -a ${CATALOG}/${spec}.yml .github/workflows/
-# fi
+for workflow in $(tr , ' ' <<<${WORKFLOWS}); do 
+  # Install the workflow
+  cp -a ${CATALOG}/${spec}.yaml .github/workflows/
+fi
