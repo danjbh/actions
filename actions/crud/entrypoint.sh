f0a5d54e43da4c4bb255f2c9c68959fce4f91897
Dan Meyers <danjbh@users.noreply.github.com>
2020-12-28 19:49:26 -0800

    A few fixes

--- actions/crud/entrypoint.sh
+++ actions/crud/entrypoint.sh
@@ -7,10 +7,10 @@ echo VERSION: $INPUT_VERSION
 echo WORKFLOWS: $INPUT_WORKFLOWS
 echo CATALOG: $INPUT_CATALOG
 
-git clone --branch ${VERSION} ${REPO}
+git clone --branch ${INPUT_VERSION} ${INPUT_REPO}
 cd actions # This needs to change... obviously.
 
-for workflow in $(tr , ' ' <<<${WORKFLOWS}); do 
+for workflow in $(tr , ' ' <<<${INPUT_WORKFLOWS}); do 
   # Install the workflow
-  cp -a ${CATALOG}/${workflow}.yaml .github/workflows/
+  cp -a ${INPUT_CATALOG}/${workflow}.yaml .github/workflows/
 fi
