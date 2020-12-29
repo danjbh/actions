0148fe853e79cdeaba0c7c62ea93072cf86398bd
Dan Meyers <danjbh@users.noreply.github.com>
2020-12-28 20:36:13 -0800

    Fixing a thing

--- actions/crud/entrypoint.sh
+++ actions/crud/entrypoint.sh
@@ -11,13 +11,5 @@ git clone --branch ${INPUT_VERSION} ${INPUT_REPO} ../actions
 
 for workflow in $(tr , ' ' <<<${INPUT_WORKFLOWS}); do 
   # Install the workflow
-  pwd
-  ls -l ..
-  ls -l ../actions
   cp -a ../actions/${INPUT_CATALOG}/${workflow}.yaml .github/workflows/
 done
-
-git add .github/workflows/
-git commit -m "Auto updating Gitflow CRUD workflows to master branch"
-git fetch origin ${GITHUB_REF##*/}
-git push origin ${GITHUB_REF##*/}
